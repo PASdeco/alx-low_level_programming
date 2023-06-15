20eb0766b91b544c69cb6def979e448f56b6cff0
Nduka <pasdeco20@gmail.com>
2023-06-15 11:22:25 -0700

    my 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Writing a program that prints a new line but this time with printf function
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n")
+		return (0);
+}
