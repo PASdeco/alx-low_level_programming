0cc57418bac9d639c4acaaf2d98d47805ec7eb07
Nduka <pasdeco20@gmail.com>
2023-06-15 11:34:43 -0700

    my 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,8 @@
  * main - Writing a program that prints a new line with printf function
  * Return: 0
  */
-
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art, \n")
+printf("with proper grammar, but the outcome is a piece of art,\n")
 return (0);
 }
