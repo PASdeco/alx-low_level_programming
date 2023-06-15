f0066ce52335a67d6cbbf4c29db7e9bfc7cb7e0d
Nduka <pasdeco20@gmail.com>
2023-06-15 11:08:30 -0700

    my 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Writing a program that prints a new line
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
