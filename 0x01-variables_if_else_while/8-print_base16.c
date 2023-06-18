0051c13e01a43d21030279fd4d0ab56dba4e9ecc
Nduka <pasdeco20@gmail.com>
2023-06-18 08:10:01 -0700

    my 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - start point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	int e;
+
+	c = 'a';
+	e = 0;
+	while
+		(e < 10) {
+			putchar(e + '0');
+			e++;
+		}
+	while
+		(c <= 'a') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
