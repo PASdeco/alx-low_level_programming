a626cde5247cf479677bce6be290aaa6f022607e
Nduka <pasdeco20@gmail.com>
2023-06-18 07:44:00 -0700

    my 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - start point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char e;
+
+	e = 'z';
+	while
+		(e >= 'a') {
+			putchar(e);
+			e--;
+		}
+	putchar('\n');
+	return (0);
+}
