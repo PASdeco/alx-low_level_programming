ab7ba3f4ffaa91650e15eb7e3068f0414ed2c043
Nduka <pasdeco20@gmail.com>
2023-06-20 14:53:16 -0700

    my gcc

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_sign - a function that prints the sign of a number
+ * @n: is the char to be printed
+ * Return: 1 and print + if n is greater than zero
+ * Returns 0 and prints 0 if n is zero
+ * Returns -1 and prints - if n is less than zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		// This is indented one level
+		_putchar('+');
+		return (1);
+	} else if (n == 0)
+		// This is indented one level
+
+	
+}
