db9862cc1142598f1130b73ca66d59e111dc70ab
Nduka <pasdeco20@gmail.com>
2023-07-11 17:27:54 -0700

    0x0B-malloc_free1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _strdup - entry point
+ * @str: input
+ * Return: Always 0
+ */
+char *_strdup(char *str)
+{
+	char *ccc;
+	int a, b = 0;
+
+	if (str == NULL)
+		return (NULL);
+	a = 0;
+	while (str[a] != '\0')
+		a++;
+
+	ccc = malloc(sizeof(char) * (a + 1));
+
+	if (ccc == NULL)
+		return (NULL);
+
+	for (b = 0; str[b]; b++)
+		ccc[b] = str[b];
+
+	return (ccc);
+}
